028fa54d6a859c33443f3996b89ac077eb9dc0c6
Kyle Lee <kylelee333@gmail.com>
2019-05-13 14:13:14 -0400

    Tweaked some things and added twitter clone logo (reversed twitter logo)

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import logo from '../Assets/twitter_flipped.png';
+import phone from '../Assets/iphone_mockup.png';
+
 const Landing = () => {
 
     const [ menu, setMenu ] = useState(false);
@@ -7,9 +10,10 @@ const Landing = () => {
     const show = (menu) ? "show" : "" 
 
     return (
-        <div className="container-fluid" style={{ padding: '2% 5%' }}>
+        <div>
+        <div className="container-fluid" style={{ padding: '2% 5%', height: '100vh' }}>
 
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
 
         <a className="navbar-brand" href="">Twitter Clone</a>
 
@@ -39,16 +43,42 @@ const Landing = () => {
       <div className="row" style={{ paddingTop: "7%" }}>
 
         <div className="col-lg-6">
-          <h1><strong>Twitter Clone</strong></h1>
+          <h1 className="animated fadeIn"><strong>Twitter Clone</strong></h1>
           <p>This is a bunch of text describing this particular twitter clone. It is a twitter clone. </p>
-          <button type="button" className="btn btn-primary btn-lg" style={{ margin: '5% 3% 5% 0' }}><i class="fab fa-apple"></i> Sign In</button>
-          <button type="button" className="btn btn-secondary btn-lg" style={{ margin: '5% 3% 5% 0' }}><i class="fab fa-google-play"></i> Sign Up</button>
+          <button type="button" className="btn btn-primary btn-lg" style={{ margin: '5% 3% 5% 0' }}><i class="fas fa-sign-in-alt"></i> Sign In</button>
+          <button type="button" className="btn btn-secondary btn-lg" style={{ margin: '5% 3% 5% 0' }}><i class="fas fa-clipboard-list"></i> Sign Up</button>
         </div>
 
         <div className="col-lg-6">
+        <img class="title-image" src={logo} alt="iphone-mockup" style={{ height: 'auto', width: '100%' }} />
         </div>
 
       </div>
+      </div>
+      <div class="container-fluid" style={{ padding: '2% 5%', backgroundColor: 'whitesmoke', height: '100vh' }}>
+
+      <div className="row">
+
+      <div className="col-lg-6">
+        <img class="title-image" src={phone} alt="iphone-mockup" style={{ height: 'auto', width: '100%' }} />
+        </div>
+
+        <div className="col-lg-6">
+          <h2 className="animated fadeIn"><strong>Share your thoughts with the world</strong></h2>
+          <br />
+          <p style={{ padding: '5% 0' }}>This is a description of some stuff that this twitter clone can do</p>
+          <p style={{ padding: '5% 0' }}>You can do tweet clones</p>
+          <p style={{ padding: '5% 0' }}>You can follow other users</p>
+          <p style={{ padding: '5% 0' }}>You can post pictures</p>
+          
+        </div>
+
+        
+
+      </div>
+
+
+    </div>
       </div>
      
     );
